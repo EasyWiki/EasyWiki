ea90ee4d7ac2a3e0e5b21235ffb1443235b07c50
Mathias Maes <mathiasmaesdev@gmail.com>
2019-11-18 09:29:49 +0100

    ADD: Config reading

--- src/modules/Config.ts
+++ src/modules/Config.ts
@@ -1,14 +1,30 @@
+import fs from 'fs';
 class Config
 {
+    private _config : any;
 
     constructor()
     {
-        //TODO: Read file
+        let configPath = "config.json";
+        
+        if(fs.existsSync("dev-config.json"))
+            configPath = "dev-config.json";
+        
+        let json = fs.readFileSync(configPath).toString();
+        this._config = JSON.parse(json);
     }
 
     public Get(path : string) : any
     {
-        //TODO: Return config path
+        let pathArr = path.split('.');
+        let currConf = this._config;
+
+        for(let p in pathArr)
+        {
+            currConf = currConf[pathArr[p]];
+        }
+
+        return currConf;
     }
 }
 
