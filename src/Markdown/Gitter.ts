5f906445d7385cf0a430ef08ff26385413156d2c
Mathias Maes <mathiasmaesdev@gmail.com>
2019-11-20 17:40:01 +0100

    ADD: Git cloning

--- src/Markdown/Gitter.ts
+++ src/Markdown/Gitter.ts
@@ -1,3 +1,13 @@
+import {Clone} from "nodegit";
+import path from 'path';
+import { Config } from "../modules/Config";
+import fs from 'fs';
+import { FileSystem } from "../modules/FileSystem";
+
+const dirPrefix = "../..";
+const pageFolder = path.join(__dirname, dirPrefix, "pages");
+const tempFolder = path.join(__dirname, dirPrefix, "pages-temp");
+
 class Gitter
 {
     public static Gitter : Gitter;
@@ -7,9 +17,15 @@ class Gitter
         Gitter.Gitter = this;
     }
 
-    public CloneRepo()
+    public async CloneRepo()
     {
-        
+        await FileSystem.RemoveFolder(tempFolder);
+
+        let cloner = Clone.clone(Config.Config.Get("Gitter.repo"), tempFolder);
+        let repo = await cloner;
+
+        await FileSystem.CopyInto(path.join(tempFolder, "pages"), pageFolder);
+        FileSystem.RemoveFolder(tempFolder);
     }
 }
 
--- src/buildMarkdown.ts
+++ src/buildMarkdown.ts
@@ -1,4 +1,10 @@
 import { MarkdownBuilder } from "./Markdown/MarkdownBuilder";
+import { Gitter } from "./Markdown/Gitter";
+import { Config } from "./modules/Config";
 
-var md = new MarkdownBuilder();
-md.BuildAll();
+var c = new Config();
+var g = new Gitter();
+g.CloneRepo();
+
+/*var md = new MarkdownBuilder();
+md.BuildAll();*/
