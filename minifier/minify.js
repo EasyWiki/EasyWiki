5bf573cc23373f7e983fd5342623db08fd21d116
Mathias Maes <mathiasmaesdev@gmail.com>
2019-12-15 19:35:59 +0100

    CHA: minifer logging

--- minifier/minify.js
+++ minifier/minify.js
@@ -1,20 +1,24 @@
 var minify = require('@node-minify/core');
-var uglifyjs = require('@node-minify/uglify-es');
+var uglifyes = require('@node-minify/uglify-es');
 
 minify({
-    compressor: uglifyjs,
+    compressor: uglifyes,
     input: './javascript/Behaviour/*.js',
     output: './public/js/behaviour.js'
+}).catch((e) => {
+    console.log("\x1b[31mFailed.\x1b[0m");
+    console.log(e);
 }).then(() => {
-    console.log("Minified Behaviour.");
+    console.log("\x1b[33mMinified Behaviour.\x1b[0m");
 });
 
 minify({
-    compressor: uglifyjs,
+    compressor: uglifyes,
     input: './javascript/UI/*.js',
     output: './public/js/ui.js'
-}).catch((e) =>{
-    console.log(e)
+}).catch((e) => {
+    console.log("\x1b[31mFailed.\x1b[0m");
+    console.log(e);
 }).then(() => {
-    console.log("Minified UI.");
+    console.log("\x1b[33mMinified UI.\x1b[0m");
 });
